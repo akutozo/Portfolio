66e8172a3e9d9be1a6da7da9e975e97105631933
Jacob Daughtry <akutozo@gmail.com>
2020-10-21 23:36:58 -0500

    Updates

---
+++ precache-manifest.9d14954ca6e2077e9215066db14341af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd68b7ac17612efd6b3c52bb0c9354b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "24edc00bbf798f76d542",
+    "url": "/portfolio/static/css/main.78e4f627.chunk.css"
+  },
+  {
+    "revision": "3bf8c3c22cf5da64eb40",
+    "url": "/portfolio/static/js/2.d3d4c26f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.d3d4c26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24edc00bbf798f76d542",
+    "url": "/portfolio/static/js/main.e3f1d90a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8254ac4c5c2ee97bea01d2932f806103",
+    "url": "/portfolio/static/media/0.8254ac4c.jpg"
+  },
+  {
+    "revision": "15fa851026f5932cec4277ebad72041b",
+    "url": "/portfolio/static/media/1.15fa8510.jpg"
+  },
+  {
+    "revision": "138927c9add4a9b6d54bc44316e80fcb",
+    "url": "/portfolio/static/media/2.138927c9.jpg"
+  },
+  {
+    "revision": "de5884bdc9a0d32a63c0fa96106cc460",
+    "url": "/portfolio/static/media/3.de5884bd.jpg"
+  },
+  {
+    "revision": "edc5ddf339ad44fea0ef474cce999572",
+    "url": "/portfolio/static/media/about-me-photo.edc5ddf3.jpg"
+  },
+  {
+    "revision": "8121994c3cde2d32f1adcd4361186648",
+    "url": "/portfolio/static/media/about-me-photo2.8121994c.jpg"
+  }
+]);
